986d4f427e28da6e45befda2980cee627fa294d7
Alan Lira <alan_lirasp@hotmail.com>
2017-08-27 23:00:10 -0300

    Created the TaxController with empty actions and created empty views

---
+++ module/Tax/config/module.config.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tax;
+
+use Zend\ServiceManager\Factory\InvokableFactory;
+use Zend\Router\Http\Segment;
+
+return [
+    'controllers' => [
+        'factories' => [
+            Controller\TaxController::class => InvokableFactory::class,
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'tax' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/tax[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\TaxController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/Tax/src/Controller/TaxController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tax\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class TaxController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
